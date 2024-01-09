4cc23608311d00a7a4a75e2b462a02cfa140fee5
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-09 20:16:16 +0700

    create menu drawer

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="font-inter grid grid-cols-12 min-h-screen bg-zinc-800">
         <div class="col-span-full md:col-span-11">
-            <div
+            <label for="my-drawer-4"
                 class="absolute right-4 top-4 md:hidden btn btn-circle bg-transparent border-zinc-600 text-white hover:text-black">
                 <Bars2Icon class="w-6" />
-            </div>
+            </label>
             <!-- slot here -->
             <div class="grid grid-cols-12 min-h-screen max-md:pt-14">
                 <div class="col-span-full md:col-span-5 lg:col-span-4 xl:col-span-3 flex flex-col justify-center p-4">
@@ -65,6 +65,50 @@
                         </a>
                     </li>
                 </ul>
+            </div>
+        </div>
+    </div>
+    <div class="drawer drawer-end">
+        <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-side">
+            <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+            <div class="px-16 py-12 w-80 min-h-full bg-zinc-800 text-gray-300">
+                <div class="mb-6 text-xl">Menu</div>
+                <ul class="menu px-0">
+                    <li>
+                        <label for="my-drawer-4" class="hover:text-white px-0">
+                            <HomeIcon class="w-4" />
+                            <div>Home</div>
+                        </label>
+                    </li>
+                    <li>
+                        <label for="my-drawer-4" class="hover:text-white px-0">
+                            <UserIcon class="w-4" />
+                            <div>Profile</div>
+                        </label>
+                    </li>
+                    <li>
+                        <label for="my-drawer-4" class="hover:text-white px-0">
+                            <BriefcaseIcon class="w-4" />
+                            <div>Projects</div>
+                        </label>
+                    </li>
+                </ul>
+                <div class="my-6 text-xl">Social</div>
+                <div class="flex gap-2">
+                    <div class="btn btn-sm btn-circle bg-transparent border-0 hover:border-2 hover:bg-transparent">
+                        <LucideInstagram :size="20" color="white" />
+                    </div>
+                    <div class="btn btn-sm btn-circle bg-transparent border-0 hover:border-2 hover:bg-transparent">
+                        <LucideLinkedin :size="20" color="white" />
+                    </div>
+                    <div class="btn btn-sm btn-circle bg-transparent border-0 hover:border-2 hover:bg-transparent">
+                        <LucideGithub :size="20" color="white" />
+                    </div>
+                    <div class="btn btn-sm btn-circle bg-transparent border-0 hover:border-2 hover:bg-transparent">
+                        <LucideGitlab :size="20" color="white" />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
