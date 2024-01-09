9c1257f2f81f3728698a8c95d31fc87b48282108
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-10 01:01:09 +0700

    create home

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-12 max-md:pt-14">
+    <div class="grid grid-cols-12 gap-10 max-md:pt-14">
         <div
             class="col-span-full md:col-span-5 lg:col-span-4 xl:col-span-3 md:max-h-screen flex flex-col justify-center p-4">
             <div class="rounded-3xl border border-zinc-600 text-white p-8 flex flex-col gap-6">
@@ -31,15 +31,36 @@
             </div>
         </div>
         <div
-            class="col-span-full md:col-span-7 lg:col-span-8 xl:col-span-9 md:max-h-screen md:overflow-y-scroll bg-indigo-500 scrollbar-hide">
-            <div class="h-96">
-                content tambahan
-            </div>
-            <div class="h-96">
-                content tambahan
-            </div>
-            <div class="h-96">
-                content tambahan
+            class="col-span-full md:col-span-7 lg:col-span-8 xl:col-span-9 md:min-h-screen md:overflow-y-scroll scrollbar-hide">
+            <div class="grid grid-cols-12 py-20 md:min-h-screen">
+                <div class="col-start-3 col-span-8 flex flex-col gap-10">
+                    <div
+                        class="rounded-2xl border border-zinc-600 flex items-center gap-2 w-min px-4 py-2 uppercase text-xs">
+                        <HomeIcon class="w-4" />
+                        <div>introduce</div>
+                    </div>
+                    <div class="text-7xl font-light">Say Hi from <span class="text-yellow-400">{{ firstname }}</span>,
+                        Webflow
+                        Designer and
+                        Developer</div>
+                    <div>I design and code beautifully simple things and i love what i do. Just simple like that! Lorem
+                        ipsum dolor sit amet consectetur adipisicing elit. Odit qui vel beatae eveniet similique.
+                        Perferendis, vel error inventore illo voluptates eaque nam harum consequatur corporis eum eius culpa
+                        cumque ex! Eum id, eos voluptas fuga et ipsam ipsa numquam unde perferendis quos ullam rem earum sed
+                        illum aperiam voluptatem. Nesciunt?</div>
+                    <div class="grow flex flex-col justify-end">
+                        <div class="flex gap-10 w-20">
+                            <div class="flex flex-col gap-2">
+                                <div class="text-7xl text-yellow-400">10+</div>
+                                <div>YEARS OF EXPERIENCE</div>
+                            </div>
+                            <div class="flex flex-col gap-2">
+                                <div class="text-7xl text-yellow-400">10+</div>
+                                <div>PROJECTS COMPLETED</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
