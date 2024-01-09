c6db8b597e2569c19ca7d4ac30bae35ea067e0cc
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-10 01:04:37 +0700

    about page

--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,7 @@
             </div>
         </div>
         <div
-            class="col-span-full md:col-span-7 lg:col-span-8 xl:col-span-9 md:min-h-screen md:overflow-y-scroll scrollbar-hide">
+            class="col-span-full md:col-span-7 lg:col-span-8 xl:col-span-9 md:max-h-screen md:overflow-y-scroll scrollbar-hide">
             <div class="grid grid-cols-12 py-20 md:min-h-screen">
                 <div class="col-start-3 col-span-8 flex flex-col gap-10">
                     <div
@@ -59,6 +59,22 @@
                                 <div>PROJECTS COMPLETED</div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-12 py-20 md:min-h-screen">
+                <div class="col-start-3 col-span-8 flex flex-col gap-10">
+                    <div
+                        class="rounded-2xl border border-zinc-600 flex items-center gap-2 w-min px-4 py-2 uppercase text-xs">
+                        <UserIcon class="w-4" />
+                        <div>About</div>
+                    </div>
+                    <div class="text-6xl font-light">Every great design begin with an even better story</div>
+                    <div>I design and code beautifully simple things and i love what i do. Just simple like that! Lorem
+                        ipsum dolor sit amet consectetur adipisicing elit. Odit qui vel beatae eveniet similique.
+                        Perferendis, vel error inventore illo voluptates eaque nam harum consequatur corporis eum eius culpa
+                        cumque ex! Eum id, eos voluptas fuga et ipsam ipsa numquam unde perferendis quos ullam rem earum sed
+                        illum aperiam voluptatem. Nesciunt?
                     </div>
                 </div>
             </div>
