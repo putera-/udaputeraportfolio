c783b6d6ef972700bbd2498389b443c68cdf7568
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-10 11:24:14 +0700

    use only lucide icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,8 @@
         <div class="col-span-full md:col-span-11">
             <label for="my-drawer-4"
                 class="absolute z-10 right-4 top-4 md:hidden btn btn-circle bg-transparent border-zinc-600 text-white hover:bg-zinc-900">
-                <Bars2Icon v-if="!show_mobile_menu" class="w-6" />
-                <XMarkIcon v-else class="w-6" />
+                <LucideMenu v-if="!show_mobile_menu" :size="20" color="#b7b7b7" />
+                <LucideX v-else :size="20" color="#b7b7b7" />
             </label>
             <!-- slot here -->
             <slot />
@@ -12,19 +12,9 @@
         <div class="max-md:hidden md:col-span-1 max-h-screen flex flex-col items-center justify-center py-10">
             <div class="rounded-full border border-zinc-600 p-2">
                 <ul class="flex flex-col items-center gap-2">
-                    <li class="tooltip tooltip-left" data-tip="Home">
+                    <li v-for="menu in menus" class="tooltip tooltip-left" :data-tip="menu.title">
                         <a href="" class="btn btn-sm btn-circle bg-transparent text-white border-0">
-                            <HomeIcon class="w-4" />
-                        </a>
-                    </li>
-                    <li class="tooltip tooltip-left" data-tip="Profile">
-                        <a href="" class="btn btn-sm btn-circle bg-transparent text-white border-0">
-                            <UserIcon class="w-4" />
-                        </a>
-                    </li>
-                    <li class="tooltip tooltip-left" data-tip="Projects">
-                        <a href="" class="btn btn-sm btn-circle bg-transparent text-white border-0">
-                            <BriefcaseIcon class="w-4" />
+                            <component :is="menu.icon" :size="16" color="#b7b7b7" />
                         </a>
                     </li>
                 </ul>
@@ -38,22 +28,10 @@
             <div class="pl-16 pr-4 pt-4 pb-12 w-80 min-h-full bg-zinc-800 text-gray-300">
                 <div class="mb-6 text-xl">Menu</div>
                 <ul class="menu px-0">
-                    <li>
+                    <li v-for="menu in menus">
                         <label for="my-drawer-4" class="hover:text-white px-2">
-                            <HomeIcon class="w-4" />
-                            <div>Home</div>
-                        </label>
-                    </li>
-                    <li>
-                        <label for="my-drawer-4" class="hover:text-white px-2">
-                            <UserIcon class="w-4" />
-                            <div>Profile</div>
-                        </label>
-                    </li>
-                    <li>
-                        <label for="my-drawer-4" class="hover:text-white px-2">
-                            <BriefcaseIcon class="w-4" />
-                            <div>Projects</div>
+                            <component :is="menu.icon" :size="16" color="#b7b7b7" />
+                            <div>{{ menu.title }}</div>
                         </label>
                     </li>
                 </ul>
@@ -78,9 +56,27 @@
 </template>
 
 <script setup lang="ts">
-import { Bars2Icon, HomeIcon, UserIcon, BriefcaseIcon, XMarkIcon } from '@heroicons/vue/24/solid'
 
 const show_mobile_menu = ref(false);
 
+interface Menu {
+    title: string
+    icon: any
+}
+
+const menus: Menu[] = [
+    {
+        title: 'Home',
+        icon: resolveComponent('LucideHome')
+    },
+    {
+        title: 'About',
+        icon: resolveComponent('LucideCircleUserRound')
+    },
+    {
+        title: 'Project',
+        icon: resolveComponent('LucideBriefcase')
+    }
+];
 
 </script>
