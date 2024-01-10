d172dc1743aac6b71c5df014d997588446f27120
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-10 11:38:53 +0700

    separate index to components

---
+++ components/index/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="grid grid-cols-12 py-20 md:min-h-screen">
+        <div class="col-span-12 lg:col-start-2 lg:col-span-10 xl:col-start-3 xl:col-span-8 flex flex-col gap-10">
+            <div class="rounded-2xl border border-zinc-600 flex items-center gap-2 w-min px-4 py-2 uppercase text-xs">
+                <LucideUser :size=16 />
+                <div>About</div>
+            </div>
+            <div class="text-6xl font-light">Every great design begin with an even better story</div>
+            <div>I design and code beautifully simple things and i love what i do. Just simple like that! Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Odit qui vel beatae eveniet similique.
+                Perferendis, vel error inventore illo voluptates eaque nam harum consequatur corporis eum eius culpa
+                cumque ex! Eum id, eos voluptas fuga et ipsam ipsa numquam unde perferendis quos ullam rem earum sed
+                illum aperiam voluptatem. Nesciunt?
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    prfile: Profile
+}>();
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
                     <LucideUser :size="200" />
                 </div>
                 <div class="text-center text-xl lg:text-2xl xl:text-2xl font-light">
-                    <div>{{ email }}</div>
+                    <div>{{ profile.email }}</div>
                     <div>Jakarta, Indonesia</div>
                 </div>
                 <div class="flex gap-2 justify-center">
@@ -32,62 +32,17 @@
         </div>
         <div
             class="col-span-full md:col-span-7 lg:col-span-8 xl:col-span-9 md:max-h-screen md:overflow-y-scroll scrollbar-hide">
-            <div class="grid grid-cols-12 py-20 md:min-h-screen">
-                <div class="col-start-3 col-span-8 flex flex-col gap-10">
-                    <div
-                        class="rounded-2xl border border-zinc-600 flex items-center gap-2 w-min px-4 py-2 uppercase text-xs">
-                        <HomeIcon class="w-4" />
-                        <div>introduce</div>
-                    </div>
-                    <div class="text-7xl font-light">Say Hi from <span class="text-yellow-400">{{ firstname }}</span>,
-                        Webflow
-                        Designer and
-                        Developer</div>
-                    <div>I design and code beautifully simple things and i love what i do. Just simple like that! Lorem
-                        ipsum dolor sit amet consectetur adipisicing elit. Odit qui vel beatae eveniet similique.
-                        Perferendis, vel error inventore illo voluptates eaque nam harum consequatur corporis eum eius culpa
-                        cumque ex! Eum id, eos voluptas fuga et ipsam ipsa numquam unde perferendis quos ullam rem earum sed
-                        illum aperiam voluptatem. Nesciunt?</div>
-                    <div class="grow flex flex-col justify-end">
-                        <div class="flex gap-10 w-20">
-                            <div class="flex flex-col gap-2">
-                                <div class="text-7xl text-yellow-400">10+</div>
-                                <div>YEARS OF EXPERIENCE</div>
-                            </div>
-                            <div class="flex flex-col gap-2">
-                                <div class="text-7xl text-yellow-400">10+</div>
-                                <div>PROJECTS COMPLETED</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="grid grid-cols-12 py-20 md:min-h-screen">
-                <div class="col-start-3 col-span-8 flex flex-col gap-10">
-                    <div
-                        class="rounded-2xl border border-zinc-600 flex items-center gap-2 w-min px-4 py-2 uppercase text-xs">
-                        <UserIcon class="w-4" />
-                        <div>About</div>
-                    </div>
-                    <div class="text-6xl font-light">Every great design begin with an even better story</div>
-                    <div>I design and code beautifully simple things and i love what i do. Just simple like that! Lorem
-                        ipsum dolor sit amet consectetur adipisicing elit. Odit qui vel beatae eveniet similique.
-                        Perferendis, vel error inventore illo voluptates eaque nam harum consequatur corporis eum eius culpa
-                        cumque ex! Eum id, eos voluptas fuga et ipsam ipsa numquam unde perferendis quos ullam rem earum sed
-                        illum aperiam voluptatem. Nesciunt?
-                    </div>
-                </div>
-            </div>
+            <IndexProfile :profile="profile" />
+            <IndexAbout :profile="profile" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { Bars2Icon, HomeIcon, UserIcon, BriefcaseIcon } from '@heroicons/vue/24/solid'
-const { firstname, lastname, email } = await $fetch('/api/profile') as Profile;
+const profile = await $fetch('/api/profile') as Profile;
 
 const fullname = computed(() => {
-    return `${firstname} ${lastname}`;
+    return `${profile.firstname} ${profile.lastname}`;
 });
 
 
