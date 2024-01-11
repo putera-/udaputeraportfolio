7677784c5495ae327896374752533c82d5983457
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-11 13:13:29 +0700

    profile card as component

--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,7 @@
     <div class="grid grid-cols-10 gap-10 max-md:pt-14">
         <div id="home"
             class="col-span-full md:col-span-4 lg:col-span-3 md:max-h-screen flex flex-col justify-center p-4 relative">
-            <div
-                class="md:fixed md:w-[33%] lg:w-[24%] rounded-3xl border border-zinc-600 text-white p-8 flex flex-col gap-6">
-                <div class="flex justify-between items-center">
-                    <div class="text-xl lg:text-2xl xl:text-3xl font-semibold text-accent">{{ fullname }}</div>
-                    <div class="text-sm font-ligbt text-right">Full Stack<br>Developer</div>
-                </div>
-                <div class="aspect-square bg-gray-300 rounded-xl flex justify-center items-center">
-                    <LucideUser :size="200" />
-                </div>
-                <div class="text-center text-xl lg:text-2xl xl:text-2xl font-light">
-                    <div>{{ profile.email }}</div>
-                    <div>Jakarta, Indonesia</div>
-                </div>
-                <div class="flex gap-2 justify-center">
-                    <div class="btn btn-circle bg-transparent border-zinc-600 text-accent">
-                        <LucideInstagram :size="20" />
-                    </div>
-                    <div class="btn btn-circle bg-transparent border-zinc-600 text-accent">
-                        <LucideLinkedin :size="20" />
-                    </div>
-                    <div class="btn btn-circle bg-transparent border-zinc-600 text-accent">
-                        <LucideGithub :size="20" />
-                    </div>
-                    <div class="btn btn-circle bg-transparent border-zinc-600 text-accent">
-                        <LucideGitlab :size="20" />
-                    </div>
-                </div>
-            </div>
+            <IndexProfileCard :profile="profile" />
         </div>
         <div class="col-span-full md:col-span-6 lg:col-span-7">
             <div class="grid grid-cols-12 md:min-h-screen">
