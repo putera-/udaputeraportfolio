334a617e44017aab3204fbfd7df2852558a3f800
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-13 10:06:07 +0700

    fix draw svg

--- components/index/skill.vue
+++ components/index/skill.vue
@@ -11,16 +11,13 @@
             <div class="mb-3 text-sm text-accent font-semibold">{{ category.title }}</div>
             <div class="flex flex-wrap gap-4 sm:gap-6 md:gap-10 items-end">
                 <div v-for="skill in category.skills" class="flex flex-col items-center">
-                    <div
-                        class="bg-slate-300 w-16 md:w-20 h-16 md:h-20 rounded-xl p-1 overflow-hidden hover:scale-110 duration-300">
-                        <component v-html="skill.svg" class="w-16"></component>
+                    <div class="bg-slate-300 w-16 md:w-20 h-16 md:h-20 rounded-xl p-1 overflow-hidden hover:scale-110 duration-300"
+                        v-html="skill.svg">
                     </div>
                     <div class="text-xs mt-2">{{ skill.title }}</div>
                 </div>
             </div>
-
         </div>
-
     </div>
 </template>
 
