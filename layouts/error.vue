9edbc68a6e4f46e845a03c26831c10aceeba7efa
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-29 19:54:48 +0700

    update 500 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,7 @@
 <template>
 <div data-theme="dracula" class="min-h-screen w-screen">
     <slot />
+    <ImagesHills class="w-[130vw] fixed -bottom-5 md:-bottom-10 -right-10 md:-right-20 lg:-right-40" />
 </div>
 </template>
 
--- pages/500.vue
+++ pages/500.vue
@@ -1,9 +1,12 @@
 <template>
 <div class="grid max-md:grid-rows-2 md:grid-cols-10 h-screen">
-    <div class="md:col-span-4 flex flex-col justify-center items-center">
-        <div>
-            <div class="text-7xl font-bold">500</div>
-            <div class="text-4xl font-semibold">Internal Server Error!</div>
+    <div class="md:col-span-4 px-10 flex flex-col">
+        <div class="py-10 text-2xl font-thin">{{ url.origin }}</div>
+        <div class="grow flex flex-col justify-center items-center">
+            <div>
+                <div class="text-6xl md:text-7xl font-bold">500</div>
+                <div class="text-4xl font-semibold">Internal Server Error!</div>
+            </div>
         </div>
     </div>
     <div class="md:col-span-6 overflow-hidden py-10">
@@ -16,4 +19,5 @@
 definePageMeta({
     layout: 'error'
 });
+const url = useRequestURL();
 </script>
