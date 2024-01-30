ec73a3e6ae1209ed6800290e532ed4bd2fb9ee95
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-30 13:13:59 +0700

    - update ui min lg for portrait - update readable date

--- components/projectCard.vue
+++ components/projectCard.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="flex justify-between items-center">
-        <div class="font-semibold text-accent text-xl mb-1">{{ project.title }}</div>
-        <div class="text-sm">{{ project.readStartDate }} - {{ project.readEndDate || "present" }}</div>
+<div class="flex flex-col">
+    <div class="font-semibold text-accent text-xl mb-1">{{ project.title }}</div>
+    <div class="text-xs">{{ project.readStartDate }} - {{ project.readEndDate }}</div>
+</div>
+<div class="aspect-video rounded-xl overflow-hidden hover:scale-105 duration-300 mt-1">
+    <img v-if="project.photos.length" :src="apiUrl + project.photos[0].path_md" alt="" class="object-cover h-full">
+    <div v-else class="aspect-video flex justify-center items-center bg-accent">
+        <LucideBriefcase :size="120" class="" />
     </div>
-    <div class="aspect-video rounded-xl overflow-hidden hover:scale-105 duration-300">
-        <img v-if="project.photos.length" :src="apiUrl + project.photos[0].path_md" alt="" class="object-cover h-full">
-        <div v-else class="aspect-video flex justify-center items-center bg-accent">
-            <LucideBriefcase :size="120" class="" />
-        </div>
-    </div>
-    <div class="font-light mt-2 flex flex-nowrap gap-2 overflow-hidden">
-        <div v-for="skill of project.ProjectSkills" class="badge badge-outline border-neutral/50 rounded-lg">{{
-            skill.skill.title }}</div>
-    </div>
+</div>
+<div class="font-light mt-2 flex flex-nowrap gap-2 overflow-hidden">
+    <div v-for="skill of project.ProjectSkills" class="badge badge-outline border-neutral/50 rounded-lg">{{
+        skill.skill.title }}</div>
+</div>
 </template>
 
 <script setup lang="ts">
