fa728ed2f3260fb1c2b46ae15c6c9ec84e330d7e
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-30 23:23:10 +0700

    ceate login page

---
+++ layouts/clear.vue
@@ -0,0 +1,20 @@
+<template>
+<div data-theme="retro" class="w-screen max-w-screee h-screen min-h-screen flex flex-col justify-center bg-secondary">
+    <div class="w-full h-full grid max-md:grid-rows-10 md:grid-cols-10 mx-auto">
+        <div
+            class="max-md:row-span-5 md:col-span-4 lg:col-span-5 xl:col-span-4 flex max-md:flex-col max-md:items-center justify-end relative">
+            <div
+                class="h-[100vh] w-full md:aspect-[9/16] bg-gradient-to-b from-accent to-primary absolute top-0 left-0 md:rounded-r-3xl shadow-2xl my-auto">
+            </div>
+            <ImagesProgrammer class="w-[60%] md:w-[75%] z-10" />
+        </div>
+        <div class="max-md:row-span-5 md:col-span-6 lg:col-span-5 xl:col-span-6 z-20">
+            <slot />
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ pages/admin/login.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="px-10 sm:px-20 lg:px-32 md:max-w-xl md:h-screen flex items-center mx-auto">
+    <div class="mx-auto w-full">
+        <div class="font-bold text-center text-3xl mb-4">Uda Putera</div>
+        <div class="flex flex-col gap-10">
+            <div>
+                <input type="text" placeholder="email"
+                    class="input w-full shadow-[0_10px_00px_12px_rgba(0,0,0,1)] focus:shadow-[0_10px_00px_12px_rgba(0,0,0,1)]">
+            </div>
+            <div>
+                <label for="">Password:</label>
+                <input type="password" placeholder="password"
+                    class="input w-full shadow-[0_10px_00px_12px_rgba(0,0,0,1)] focus:shadow-[0_10px_00px_12px_rgba(0,0,0,1)]">
+            </div>
+            <div class="text-right">
+                <button class="btn btn-base-100 w-min shadow-[0_7px_00px_8px_rgba(0,0,0,1)]">LOGIN</button>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'clear'
+})
+
+</script>
