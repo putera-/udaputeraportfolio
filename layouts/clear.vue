c078a03590d96c9890ed3184a8487e93a563f2e2
Rizjami Putera <rizjamiputera@gmail.com>
2024-02-01 00:46:17 +0700

    update login ui

--- layouts/clear.vue
+++ layouts/clear.vue
@@ -1,15 +1,24 @@
 <template>
-<div data-theme="retro" class="w-screen max-w-screee h-screen min-h-screen flex flex-col justify-center bg-secondary">
-    <div class="w-full h-full grid max-md:grid-rows-10 md:grid-cols-10 mx-auto">
-        <div
-            class="max-md:row-span-5 md:col-span-4 lg:col-span-5 xl:col-span-4 flex max-md:flex-col max-md:items-center justify-end relative">
+<div data-theme="nord"
+    class="w-screen max-w-screen h-screen min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-gray-600 to-gray-500 p-6 md:p-10 lg:p-16 xl:p-20">
+    <div
+        class="max-w-7xl lg:w-screen max-lg:aspect-[9/16] lg:aspect-video rounded-3xl overflow-hidden bg-white p-3 md:p-4">
+        <div class="w-full h-full grid max-lg:grid-rows-10 lg:grid-cols-10 relative rounded-3xl bg-base-100">
+
+            <!-- absolute bg -->
             <div
-                class="h-[100vh] w-full md:aspect-[9/16] bg-gradient-to-b from-accent to-primary absolute top-0 left-0 md:rounded-r-3xl shadow-2xl my-auto">
+                class="h-full max-lg:w-full aspect-[9/16] max-w-full bg-gradient-to-b from-accent to-primary absolute top-0 left-0 rounded-2xl lg:rounded-3xl shadow-2xl z-0">
             </div>
-            <ImagesProgrammer class="w-[60%] md:w-[75%] z-10" />
-        </div>
-        <div class="max-md:row-span-5 md:col-span-6 lg:col-span-5 xl:col-span-6 z-20">
-            <slot />
+
+            <div
+                class="max-lg:row-span-4 lg:col-span-5 xl:col-span-4 h-full flex max-lg:flex-col max-lg:items-center mt-12 md:mt-20 lg:mt-0 max-lg:ml-10 justify-end">
+
+                <ImagesProgrammer class="w-[60%] md:w-[75%] z-10" />
+
+            </div>
+            <div class="max-lg:row-span-6 lg:col-span-5 xl:col-span-6 h-full rounded-2xl z-30">
+                <slot />
+            </div>
         </div>
     </div>
 </div>
--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,19 +1,24 @@
 <template>
-<div class="px-10 sm:px-20 lg:px-32 md:max-w-xl md:h-screen flex items-center mx-auto">
+<div class="px-10 sm:px-20 lg:px-24 xl:px-44 h-full flex items-center mx-auto">
     <div class="mx-auto w-full">
         <div class="font-bold text-center text-3xl mb-4">Uda Putera</div>
-        <div class="flex flex-col gap-10">
-            <div>
+        <div class="flex flex-col gap-5 md:gap-10">
+            <div
+                class="join items-center bg-base-100 pr-2 shadow-[0_7px_00px_8px_rgba(0,0,0,1)] focus:shadow-[0_5px_00px_6px_rgba(0,0,0,1)] md:shadow-[0_10px_00px_12px_rgba(0,0,0,1)] md:focus:shadow-[0_9px_00px_10px_rgba(0,0,0,1)] duration-200">
                 <input type="text" placeholder="email"
-                    class="input w-full shadow-[0_10px_00px_12px_rgba(0,0,0,1)] focus:shadow-[0_10px_00px_12px_rgba(0,0,0,1)]">
+                    class="join-item input max-sm:input-sm w-full focus:border-0 focus:outline-0">
+                <LucideMailCheck :size="36" class="join-item" />
             </div>
-            <div>
-                <label for="">Password:</label>
+            <div
+                class="join items-center bg-base-100 pr-2 shadow-[0_7px_00px_8px_rgba(0,0,0,1)] focus:shadow-[0_5px_00px_6px_rgba(0,0,0,1)] md:shadow-[0_10px_00px_12px_rgba(0,0,0,1)] md:focus:shadow-[0_9px_00px_10px_rgba(0,0,0,1)] duration-200">
                 <input type="password" placeholder="password"
-                    class="input w-full shadow-[0_10px_00px_12px_rgba(0,0,0,1)] focus:shadow-[0_10px_00px_12px_rgba(0,0,0,1)]">
+                    class="join-item input max-sm:input-sm w-full focus:border-0 focus:outline-0">
+                <LucideKeySquare :size="36" class="join-item" />
+
             </div>
-            <div class="text-right">
-                <button class="btn btn-base-100 w-min shadow-[0_7px_00px_8px_rgba(0,0,0,1)]">LOGIN</button>
+            <div class="text-right max-sm:mt-2">
+                <button
+                    class="btn max-sm:btn-sm btn-base-100 w-min shadow-[0_5px_00px_6px_rgba(0,0,0,1)] md:shadow-[0_7px_00px_8px_rgba(0,0,0,1)]">LOGIN</button>
             </div>
         </div>
     </div>
