8437f23b8e336d97eae0c5ca6168c314c03099c2
Rizjami Putera <rizjamiputera@gmail.com>
2024-02-20 00:52:19 +0700

    update validation

--- utils/blog.validation.ts
+++ utils/blog.validation.ts
@@ -1,10 +1,6 @@
 import Joi from "joi";
 
 export const isBlog = Joi.object({
-    title: isString.required(),
-    content: isText.required(),
-    photos: Joi.array().items(Joi.object({
-        id: Joi.number(),
-        index: Joi.number()
-    })).label('Photos')
+    title: isString.required().label("Title"),
+    content: isText.required().label("Content")
 });
