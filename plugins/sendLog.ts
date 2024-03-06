43d5b1c8a9efbca20e8f4974562cbf8265c3d388
Rizjami Putera <rizjamiputera@gmail.com>
2024-03-06 21:21:52 +0700

    remove get ip address on initial

--- plugins/sendLog.ts
+++ plugins/sendLog.ts
@@ -2,11 +2,6 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     const LogStore = useLogStore();
     const router = useRouter();
 
-    // get ip address on initial
-    router.isReady().then(async () => {
-        await LogStore.getIpAddress();
-    });
-
     // send web access log on path change
     router.afterEach(async (to, from) => {
         if (!to.fullPath.includes('/admin')) {
