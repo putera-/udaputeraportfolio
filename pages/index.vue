04571c913ab71b968a64b35860cbfb898cb85d3f
Rizjami Putera <rizjamiputera@gmail.com>
2024-09-15 00:09:44 +0700

    add setup photo

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,25 @@
 <template>
-<div class="grid grid-cols-10 gap-10 max-md:pt-14">
-    <div id="home"
-        class="col-span-full md:col-span-5 lg:col-span-4 xl:col-span-3 md:max-h-screen flex flex-col justify-center px-8 relative">
-        <IndexProfileCard :profile="profile" />
-    </div>
-    <div class="col-span-full md:col-span-5 lg:col-span-6 xl:col-span-7">
-        <div class="grid grid-cols-12 md:min-h-screen">
-            <div
-                class="col-span-full lg:col-start-2 lg:col-span-10 xl:col-start-3 xl:col-span-8 flex flex-col gap-10 max-lg:px-4">
-                <IndexIntro :profile="profile" id="intro" />
-                <!-- <IndexAbout :profile="profile" id="about" /> -->
-                <IndexBlog v-if="useShowBlog" :blogs="blogs" id="blogs" />
-                <IndexEducation v-if="useShowEdu" :educations="educations" id="education" />
-                <IndexExperience v-if="useShowExp" :experiences="experiences" id="experience" />
-                <IndexSkill v-if="useShowSkill" :skills="skills" id="skills" />
-                <IndexProject v-if="useShowProject" :projects="projects" id="projects" />
+    <div class="grid grid-cols-10 gap-10 max-md:pt-14">
+        <div id="home"
+            class="col-span-full md:col-span-5 lg:col-span-4 xl:col-span-3 md:max-h-screen flex flex-col justify-center px-8 relative">
+            <IndexProfileCard :profile="profile" />
+        </div>
+        <div class="col-span-full md:col-span-5 lg:col-span-6 xl:col-span-7">
+            <div class="grid grid-cols-12 md:min-h-screen">
+                <div
+                    class="col-span-full lg:col-start-2 lg:col-span-10 xl:col-start-3 xl:col-span-8 flex flex-col gap-10 max-lg:px-4">
+                    <IndexIntro :profile="profile" id="intro" />
+                    <!-- <IndexAbout :profile="profile" id="about" /> -->
+                    <IndexBlog v-if="useShowBlog" :blogs="blogs" id="blogs" />
+                    <IndexEducation v-if="useShowEdu" :educations="educations" id="education" />
+                    <img src="/setup-2024.jpeg" alt="Uda Putera Remote Gear 2024" class="rounded-lg">
+                    <IndexExperience v-if="useShowExp" :experiences="experiences" id="experience" />
+                    <IndexSkill v-if="useShowSkill" :skills="skills" id="skills" />
+                    <IndexProject v-if="useShowProject" :projects="projects" id="projects" />
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script setup lang="ts">
